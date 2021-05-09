89abfd3cfe5513a54a314860809ad5d6be5708c7
Andrei Lineischikov <code@lineischikov.icu>
2021-05-09 23:05:14 +0300

    fix transalations

--- screens/transactions/AddTransactionButton.tsx
+++ screens/transactions/AddTransactionButton.tsx
@@ -6,37 +6,39 @@ import {IActionProps} from '../../lib/react-native-floating-action';
 import {useNavigatorThemeColors} from '../../themes';
 import {TransactionsScreenNavigationProp} from '../../types';
 import {TransactionType} from './transaction-type';
+import {useTranslation} from 'react-i18next';
 
 export interface AddTransactionButtonProps {}
 
 export const AddTransactionButton: React.FC<AddTransactionButtonProps> = (props) => {
   const {primary} = useNavigatorThemeColors();
+  const {t} = useTranslation();
 
   const actions = useMemo<IActionProps[]>(() => {
     return [
       {
-        text: TransactionType.Expense,
+        text: t('Screen.AddTransaction.Expense'),
         color: primary,
         icon: <MinusIcon color={'white'} size={24} />,
         name: TransactionType.Expense,
         position: 1,
       },
       {
-        text: TransactionType.Income,
+        text: t('Screen.AddTransaction.Income'),
         color: primary,
         icon: <PlusIcon color={'white'} size={24} />,
         name: TransactionType.Income,
         position: 2,
       },
       {
-        text: TransactionType.Transfer,
+        text: t('Screen.AddTransaction.Transfer'),
         color: primary,
         icon: <SwapHorizIcon color={'white'} size={24} />,
         name: TransactionType.Transfer,
         position: 2,
       },
     ];
-  }, [primary]);
+  }, [primary, t]);
 
   const nav = useNavigation<TransactionsScreenNavigationProp>();
 
