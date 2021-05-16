579d1aa283fe5a0365fe8b75bf820b13e29436e7
Andrei Lineischikov <code@lineischikov.icu>
2021-05-16 23:31:33 +0300

    fix typings

--- @types/declarations.d.ts
+++ @types/declarations.d.ts
@@ -1,11 +1,4 @@
-declare module '*.svg' {
-  import React from 'react';
-  import {SvgProps} from 'react-native-svg';
-  const content: React.FC<SvgProps>;
-  export default content;
-}
-
-import {resources} from './init-i18n';
+import {resources} from '../../init-i18n';
 
 declare module 'react-i18next' {
   type DefaultResources = typeof resources['en'];
