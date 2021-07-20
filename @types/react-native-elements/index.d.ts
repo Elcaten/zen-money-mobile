f98bbb16587a32c734f5bf707542cc18b8c99996
Andrei Lineischikov <code@lineischikov.icu>
2021-07-20 15:26:38 +0300

    Use ts 4.1 so that react-i18next typings work

--- @types/react-native-elements/index.d.ts
+++ @types/react-native-elements/index.d.ts
@@ -1,10 +1,3 @@
-import {resources} from '../init-i18n';
-
-declare module 'react-i18next' {
-  type DefaultResources = typeof resources['en'];
-  interface Resources extends DefaultResources {}
-}
-
 declare module 'react-native-elements' {
   import React, {TextInputProps} from 'react';
 
