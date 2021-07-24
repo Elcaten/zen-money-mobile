0de9b7379a45dc69ee0ab7f15cf56eff7d9e6108
Andrei Lineischikov <code@lineischikov.icu>
2021-07-24 20:38:16 +0300

    Bug: Fab button is not closed when navigating to EditTransactoinScreen

--- screens/transactions/TransactionsScreen/AddTransactionButton.tsx
+++ screens/transactions/TransactionsScreen/AddTransactionButton.tsx
@@ -69,20 +69,29 @@ export const AddTransactionButton: React.FC<AddTransactionButtonProps> = (props)
           icon: 'minus',
           style: {backgroundColor: primary},
           label: t('TransactionType.Expense'),
-          onPress: () => nav.navigate('EditTransactionScreen', {transactionType: TransactionType.Expense}),
+          onPress: () => {
+            setOpen(false);
+            nav.navigate('EditTransactionScreen', {transactionType: TransactionType.Expense});
+          },
           small: false,
         },
         {
           icon: 'plus',
           style: {backgroundColor: primary},
           label: t('TransactionType.Income'),
-          onPress: () => nav.navigate('EditTransactionScreen', {transactionType: TransactionType.Income}),
+          onPress: () => {
+            setOpen(false);
+            nav.navigate('EditTransactionScreen', {transactionType: TransactionType.Income});
+          },
         },
         {
           icon: 'swap-horizontal',
           style: {backgroundColor: primary},
           label: t('TransactionType.Transfer'),
-          onPress: () => nav.navigate('EditTransactionScreen', {transactionType: TransactionType.Transfer}),
+          onPress: () => {
+            setOpen(false);
+            nav.navigate('EditTransactionScreen', {transactionType: TransactionType.Transfer});
+          },
         },
       ]}
       onStateChange={onStateChange}
